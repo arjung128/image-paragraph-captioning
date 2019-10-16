5b5d69971f3bac6a45deae2bc00a10fc58cbd140
arjung128 <arjung2@illinois.edu>
2019-10-15 23:07:06 -0500

    added model files

---
+++ models/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import copy
+
+import numpy as np
+import misc.utils as utils
+import torch
+
+from .AttModel import TopDownModel
+# from .AttModel import vectorModel
+from .AttModel import vectorCore
+
+def setup(opt):
+    model = TopDownModel(opt)
+
+    # Check compatibility if training is continued from previously saved model
+    if vars(opt).get('start_from', None) is not None:
+        # check if all necessary files exist 
+        assert os.path.isdir(opt.start_from)," %s must be a a path" % opt.start_from
+        assert os.path.isfile(os.path.join(opt.start_from,"infos_"+opt.id+".pkl")),"infos.pkl file does not exist in path %s"%opt.start_from
+        # model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model.pth')))
+
+    return model
+
+def setup_vectorModel():
+    # model = vectorModel(opt)
+    model = vectorCore()
+    
+    return model
